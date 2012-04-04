b2509b308c499cf3bf4baa45a01976cebc988473
Dirk Dokter <dirk@dirkdokter.nl>
2012-04-04 11:23:08 +0200

    typo in pprz_algebra_int.h

--- sw/airborne/math/pprz_algebra_int.h
+++ sw/airborne/math/pprz_algebra_int.h
@@ -52,7 +52,7 @@ struct Int16Vect3 {
 #define INT32_POS_OF_CM 2.56
 #define INT32_POS_OF_CM_NUM 64
 #define INT32_POS_OF_CM_DEN 25
-s
+
 #define INT32_SPEED_FRAC 19
 #define INT32_SPEED_OF_CM_S 5242.88
 #define INT32_SPEED_OF_CM_S_NUM 41943
