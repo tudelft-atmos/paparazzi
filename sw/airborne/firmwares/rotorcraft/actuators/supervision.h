db9713cd0677ecdcdaeefeb3fe99c26db61355f2
Dirk Dokter <dirk@dirkdokter.nl>
2012-04-04 15:02:42 +0200

    created new branch for hover props off

--- sw/airborne/firmwares/rotorcraft/actuators/supervision.h
+++ sw/airborne/firmwares/rotorcraft/actuators/supervision.h
@@ -37,6 +37,7 @@ struct Supervision {
   bool_t override_enabled[SUPERVISION_NB_MOTOR];
   int32_t override_value[SUPERVISION_NB_MOTOR];
   uint32_t nb_failure;
+  bool_t override_by_module;
 };
 
 extern struct Supervision supervision;
---
+++ sw/airborne/modules/transitioning_vehicles/hoverPropsOff.c
@@ -0,0 +1,12 @@
+#include "modules/transitioning_vehicles/hoverPropsOff.h"
+#include "generated/airframe.h"
+#include "firmwares/rotorcraft/autopilot.h"
+#include "firmwares/rotorcraft/actuators/supervision.h"
+
+void transveh_hover_props_off_init(void) {
+
+}
+
+void transveh_hover_props_off_periodic(void) {
+
+}
